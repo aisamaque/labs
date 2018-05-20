427d9edefbafcf61a964b5ba8570ea3594e26526
Aisamaque Santos <aisamaque.santos@gmail.com>
2018-05-20 11:53:40 -0300

    Update Program.cs

--- WorkingWithBigNumbers/WorkingWithBigNumbers/Program.cs
+++ WorkingWithBigNumbers/WorkingWithBigNumbers/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Diagnostics;
 using System.Numerics;
 
 namespace WorkingWithBigNumbers
@@ -7,16 +8,41 @@ namespace WorkingWithBigNumbers
     {
         static void Main(string[] args)
         {
-            //BigIntegers requires System.Numerics;
             int number = 1024;
             int exponent = 2048;
+            BigInteger result = new BigInteger();
 
-            BigInteger result = BigInteger.Pow(number, exponent);
-           
+            Statistic a = RunWithSatistics(() =>
+            {
+                result = BigInteger.Pow(number, exponent);
+            }, "Pow");
+
+            Console.WriteLine("Result:");
             Console.WriteLine(result.ToString());
             Console.WriteLine("#######################");
+            Console.WriteLine("ElapsedTime");
+            Console.WriteLine("Seconds: {0}", a.Time.TotalSeconds);
             Console.WriteLine("Press any key to Exit");
             Console.ReadKey();
+        }
+
+        public static Statistic RunWithSatistics(Action Run, string StatisticName)
+        {
+            Stopwatch time = Stopwatch.StartNew();
+            Run.Invoke();
+            time.Stop();
+            return new Statistic { Name = StatisticName, Time = time.Elapsed };
+        }
+    }
+
+    public class Statistic
+    {
+        public string Name { get; set; }
+        public TimeSpan Time { get; set; }
+
+        public Statistic()
+        {
+
         }
     }
 }

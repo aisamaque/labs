51ff75ecdb2831cbb6a051819f0953785c9614e0
Aisamaque Santos <aisamaque.santos@gmail.com>
2018-05-20 09:12:20 -0300

    Add files via upload

---
+++ WorkingWithBigNumbers/WorkingWithBigNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace WorkingWithBigNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BigIntegers requires System.Numerics;
+            int number = 1024;
+            int exponent = 2048;
+
+            BigInteger result = BigInteger.Pow(number, exponent);
+           
+            Console.WriteLine(result.ToString());
+            Console.WriteLine("#######################");
+            Console.WriteLine("Press any key to Exit");
+            Console.ReadKey();
+        }
+    }
+}
